4f57b437958fb5377a11d80f42fc8d667599ae40
Henrik Selje Bygnes <31667488+henriksb@users.noreply.github.com>
2023-12-20 15:38:25 +0100

    Update decrypt.sh

--- Dag 3/decrypt.sh
+++ Dag 3/decrypt.sh
@@ -1,1 +1,3 @@
-openssl enc -aes-192-ctr -d -in huskeliste.txt.enc -out decrypted_file -K dda2846b010a6185b5e76aca4144069f88dc7a6ba49bf128 -iv 4867746e617466497278676265313233
+#!/bin/bash
+
+openssl enc -aes-192-ctr -d -in huskeliste.txt.enc -out decrypted_file -K dda2846b010a6185b5e76aca4144069f88dc7a6ba49bf128 -iv 4867746e617466497278676265313233 | cat decrypted_file
