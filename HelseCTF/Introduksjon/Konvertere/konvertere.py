608df48e518e90150ce922c25400af8944435ba7
Henrik Selje Bygnes <31667488+henriksb@users.noreply.github.com>
2024-01-25 05:10:13 +0100

    Add files via upload

---
+++ HelseCTF/Introduksjon/Konvertere/konvertere.py
@@ -0,0 +1,3 @@
+from Crypto.Util.number import long_to_bytes, bytes_to_long   
+flag = long_to_bytes(b"9999168102934914777774346849293018679871929920575661949")
+print(flag)
---
+++ HelseCTF/maldocs/pass_or_fail/deobfuscated_bat.bat
@@ -0,0 +1,24 @@
+@echo off
+set "饿饿尔艾=wrUnsT7NBeCfiljYRb5cPF2oQ8SWZzG@VJpvXxO6K3HI LadgAqDmu14h=MEkty09"
+@set "斯尔色斯=Kt@4oE2uzLIZ80qiR1YpOldD SjNe7bWskfvUMCwVBnrGgFHamPJhxXAyQ536c9=T"
+@set "饿阿豆豆=ybVIQN=SoEOikuHCcfa2 LhB3KUA6rzXxY0Dw7nJg4sM9tjG85d1ZqePlWRv@FTpm"
+@set "尔色爱德=NaY35kTOpHZ@2E0hI=yrxjm SUbKF6eiCW7R9fslt8wJuzdoDLA1Gv4nQgMXVPcBq"
+@set "斯尔贝贝=RzxkNpCdKhS3@2efquTcowElvU8W9rLHVa 6AZDG7Q0Yy5JijnFO4MPbgXmBtI1s="
+for /l %%y in (1 1 1) do @echo off
+if 1 EQU 1 set "abc2=https://youtu.be/YZf0Q-v3u-k?feature=6e6569746865722069732074686973"
+for /l %%y in (1 1 1) do set "abc3=https://youtu.be/3xYXUeSmb-Y?feature=54686973206973206e6f742074686520666c6167"
+if 46 EQU 0 (@exit) else set "abc4=G@nd@lf"
+if 1 EQU 1 set "abc1=https://youtu.be/qybUFnY7Y8w?feature="
+if 1 LsS 109 set "abc7=explorer.exe"
+if 1 EQU 1 if not "" equ "" goto usage
+for /l %%y in (1 1 1) do goto end
+:usage  lliiliiilil   llililllili
+if 1 LsS 109 set "abc5=68656c73656374667b746869"
+if 1 LsS 109 if == (set "abc6=735f325f7368616c6c5f706173737d"
+if 1 LsS 109 set "abc7="
+) else (start )
+if 1 LsS 109 start 
+if 1 LsS 109 exit
+:end  lilllillill   liillllllil
+for /l %%y in (1 1 1) do start 
+
