8f873486c7b51bda79770a298632f7ed68e093b7
Henrik Selje Bygnes <31667488+henriksb@users.noreply.github.com>
2024-01-25 05:27:53 +0100

    Delete HelseCTF/maldocs/Claim_in_mail directory

--- HelseCTF/maldocs/Claim_in_mail/Oppgave/f1421de8-0e36-4938-a753-849d211995d4/ibidem.cmd
+++
@@ -1,5 +0,0 @@
-set besiegingWaterproofing=naststargysystemx
-set yawsAbolitionists=%besiegingWaterproofing:~4,4%
-set counselWaltzer=%besiegingWaterproofing:~10,6%
-set flightorfight=%besiegingWaterproofing:~14,1%%besiegingWaterproofing:~16,1%%besiegingWaterproofing:~14,1%
-start /min respondents\suspect.cmd %counselWaltzer% %yawsAbolitionists% %flightorfight%
