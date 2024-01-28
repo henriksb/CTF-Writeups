b4eb8ff3e02ac87a230494b2095c0184c150c0a9
Henrik Selje Bygnes <31667488+henriksb@users.noreply.github.com>
2024-01-28 05:58:24 +0100

    Update solve.py

--- HelseCTF2024/matte_og_krypto/Joppe 2/solve.py
+++ HelseCTF2024/matte_og_krypto/Joppe 2/solve.py
@@ -1,7 +1,7 @@
 import sympy as sp
+from Crypto.Util.number import long_to_bytes
 
-# The provided parts of the secret
-parts = [
+hemmeligheter = [
     (4745696227450807, 655305480794027129181307180701455045712682321660286466368078),
     (2588682506107567, 655305480793967733879479427128553132958736140573542016023878),
     (4035090358829972, 655305480794003703185282913192696782073761367365025193477153)
@@ -9,7 +9,7 @@ parts = [
 
 # Lagrange Interpolation to find the polynomial
 x = sp.symbols('x')
-polynomial = sum([y * sp.prod([(x - xj) / (xi - xj) for xj in [part[0] for part in parts if part[0] != xi]]) for xi, y in parts])
+polynomial = sum([y * sp.prod([(x - xj) / (xi - xj) for xj in [h[0] for h in hemmeligheter if h[0] != xi]]) for xi, y in hemmeligheter])
 
 # Simplify the polynomial
 simplified_polynomial = sp.simplify(polynomial)
@@ -17,18 +17,7 @@ simplified_polynomial = sp.simplify(polynomial)
 # Evaluate the polynomial at x = 0 to find the secret
 secret = simplified_polynomial.subs(x, 0)
 
-def number_to_text(number):
-    """ Convert a large number into a string assuming it's ASCII encoded """
-    text = ""
-    while number > 0:
-        # Extract the least significant byte (8 bits)
-        byte = number & 0xff
-        # Convert the byte to a character and prepend it to the text
-        text = chr(byte) + text
-        # Shift the number 8 bits to the right
-        number >>= 8
-    return text
 
 # Convert the number to text
-decoded_text = number_to_text(secret)
+decoded_text = long_to_bytes(secret)
 print(decoded_text)
