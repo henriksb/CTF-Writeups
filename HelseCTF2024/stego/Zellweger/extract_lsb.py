d6928db224fe823df7545734dd4240e557015b35
Henrik Selje Bygnes <31667488+henriksb@users.noreply.github.com>
2024-02-03 11:28:19 +0100

    Update extract_lsb.py

--- HelseCTF2024/stego/Zellweger/extract_lsb.py
+++ HelseCTF2024/stego/Zellweger/extract_lsb.py
@@ -2,6 +2,9 @@ from PIL import Image
 import numpy as np
 
 def extract_following_bits(lsb_string, sequence_length=10):
+    """Hent binærteksten som inneholder flagget.
+        Dette må bli konvertert fra decabit til tekst
+        for å få flagget. https://www.dcode.fr/decabit-code"""
     extracted_sequence = ""
     index = 0
     while index < len(lsb_string):
@@ -17,7 +20,7 @@ def extract_following_bits(lsb_string, sequence_length=10):
     return extracted_sequence
 
 def extract_lsb(pixel):
-    # pixel is a tuple with (R, G, B), we need to get the LSB from each component
+    # piksel er en tupple med (R, G, B), vi må få LSB fra hver del
     return (pixel[0] & 1, pixel[1] & 1, pixel[2] & 1)
 
 
@@ -26,15 +29,16 @@ img = Image.open(img_path)
 
 img_data = np.array(img)
 
-# Extract LSBs for the entire image
+# Ekstraher LSB fra hele bildet
 lsb_data = np.apply_along_axis(extract_lsb, 2, img_data)
 
-# Since the image can be large, and we want to return a text-based representation,
-# we'll need to flatten the lsb_data and convert it to a string of 0s and 1s.
-# We'll concatenate the LSBs for R, G, B for each pixel.
+# Siden bildet kan være stort, og vi ønsker og returnere en tekstbasert representasjon,
+# må vi flate ut lsb_data og konvertere den til en streng med 0-er og 1-ere.
+# Så setter vi sammen LSB-ene for R,G,B for hver piksel.
 lsb_flat = lsb_data.flatten()
 lsb_str = ''.join(map(str, lsb_flat))
 
+# Korter ned på den enorme mengden data
 binary = lsb_str[0:27000]
 
 print(extract_following_bits(binary))
